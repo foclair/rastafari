7659b31d847cac206c2a5d98edebef31da7838f3
Mattias Jakobsson <mattias.jakobsson@smhi.se>
2023-07-26 16:21:48 +0200

    Add type annotations

---
+++ src/rastafari/core.pyi
@@ -0,0 +1,26 @@
+import numpy as np
+import numpy.typing as npt
+
+from . import ExtentType
+
+WeightsDict = dict[tuple[int, int], float]
+
+def ddaf_line_subpixel(
+    x0: float,
+    y0: float,
+    x1: float,
+    y1: float,
+    weights: WeightsDict,
+    length: float,
+    grid_extent: ExtentType,
+    grid_dx: float,
+    grid_dy: float,
+) -> int | None: ...
+def even_odd_polygon_fill(
+    points: npt.NDArray[np.float_],
+    weights: WeightsDict,
+    grid_extent: ExtentType,
+    grid_nx: int,
+    grid_ny: int,
+    subgridcells: int = 2,
+) -> None: ...
