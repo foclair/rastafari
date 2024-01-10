910c9215699b009a616529471bbbd1f36e8cf50e
David Segersson <david.segersson@smhi.se>
2024-01-10 10:42:57 +0100

    fixed bug

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 from setuptools import Extension, setup
 USE_CYTHON = os.environ.get("RASTAFARI_USE_CYTHON", False)
-print(f"USE_CYTHON: {USE_CYTHON}"
+print(f"USE_CYTHON: {USE_CYTHON}")
 ext = "pyx" if USE_CYTHON else "c"
 extensions = [
     Extension("rastafari.core", [f"src/rastafari/core.{ext}"]),
